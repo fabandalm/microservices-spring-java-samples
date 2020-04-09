8ba25196f45f2393c44adeea796921977d35f2e3
Almeida <falmeida.tech@gmail.com>
2020-04-09 17:37:24 -0400

    Add new features

---
+++ currency-exchange-service/src/main/java/com/falmeida/tech/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.falmeida.tech.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long>{
+
+	ExchangeValue findByFromAndTo(String from,String to);
+	
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into exchange_value (id,currency_from,currency_to,conversion_multiple,port)
+values (1001,'USS','INR',65,0);
+insert into exchange_value (id,currency_from,currency_to,conversion_multiple,port)
+values (1002,'BRL','CAD',72,0);
+insert into exchange_value (id,currency_from,currency_to,conversion_multiple,port)
+values (1003,'USS','EUR',89,0);
+
+
