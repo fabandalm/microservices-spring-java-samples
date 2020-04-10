d209e7d7e2b67e7a7ad763bd602c8b1b1dcce22c
Almeida <falmeida.tech@gmail.com>
2020-04-10 11:40:00 -0400

    Initial Commit

---
+++ netflix-eureka-naming-server/src/test/java/com/falmeida/tech/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falmeida.tech.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
